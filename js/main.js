9b55ffd6b295836e88e7c0ed033efe59a12bbf2a
黄小红 <hxh@localhost.(none)>
2016-10-29 15:29:54 +0800

    add api

--- js/main.js
+++ js/main.js
@@ -1,8 +1,37 @@
+var APP;
 $(function() {
-  new Vue({
+  APP = new Vue({
     el: 'body',
     data: {
-      hello: 'world'
+      repos: []
     }
   })
+
+
+  init()
 })
+
+function init() {
+  $.post('http://192.168.141.128:3000/api/latest', {}, function(data) {
+    data.items.forEach(function(item) {
+      item.fresh = freshData(item.pushed_at)
+    })
+    APP.repos = data.items
+  })
+}
+
+
+// 获取更新频率
+function freshData(time) {
+  var diff = (Date.now() - Date.parse(time)) / 3600000
+  if (diff > 60) {
+    return ['outdated', '过期']
+  }
+
+  if (_val < 7) {
+    return ['often', '频繁']
+  }
+  
+
+  return ['normal', '正常']
+}
